9d0216d9486a992094e1a75d45127e3ccb584693
Nakib Saleh <nakibsaleh@iut-dhaka.edu>
2024-09-18 09:11:06 +0600

    First And Second task done

---
+++ Lab 3/220_task_1.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Counter{
+    private:
+    int count=0;
+    int incrementAmount=0;
+
+    public:
+    
+    void setIncrementStep(int n){
+        incrementAmount = n;
+    }
+    int getCount(){
+        return count;
+    }
+
+    void increment(){
+        count+=incrementAmount;
+    }
+    void resetCount(){
+        count=0;
+    }
+
+};
+
+int main(){
+    int incri =4;
+
+    Counter c1;
+    cout<<c1.getCount()<<endl;
+
+    c1.increment();
+    cout<<c1.getCount()<<endl;
+
+    c1.setIncrementStep(incri);
+    c1.increment();
+    cout<<c1.getCount()<<endl;
+
+    c1.resetCount();
+    cout<<c1.getCount()<<endl;
+}
---
+++ Lab 3/220_task_2.cpp
@@ -0,0 +1,54 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class RationalNumber{
+    int _numerator,_denominator;
+
+    public:
+    
+    void assign(int numerator, int denominator){
+        _numerator=numerator;
+
+        if(denominator==0){
+            cout<<"You can not assign 0 as denominator"<<endl;
+        }
+        else{
+            _denominator = denominator;
+        }
+    }
+
+    float convert(){
+        return ((float)_numerator/_denominator );
+    }
+
+    void invert(){
+        int temp= _numerator;
+        if(temp == 0){
+           cout<<"You can not assign 0 as denominator.Inversion Failed"<<endl; 
+        }
+        else{
+          _numerator = _denominator;
+          _denominator = temp;
+        }
+    }
+
+    void print(){
+        cout<< "The Rational Number is "<<_numerator <<"/"<<_denominator<<endl;
+    }
+
+};
+
+int main(){
+    RationalNumber r1;
+    r1.assign(3,0);
+
+    r1.assign(3,2);
+    cout<<r1.convert()<<endl;
+
+    r1.assign(0,2);
+    r1.print();
+    r1.invert();
+    r1.print();
+
+
+}
